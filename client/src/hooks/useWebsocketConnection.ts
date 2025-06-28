253cf1c9c5b8e44e1054c4606b2a986e734d2330
Musfiqur Sakib <m.r.sakib@outlook.com>
2025-06-28 18:30:29 +1000

    fix typo and added logging

--- client/src/hooks/useWebsocketConnection.ts
+++ client/src/hooks/useWebsocketConnection.ts
@@ -6,13 +6,17 @@ import { actions as AppAction } from '../slices/app-state';
 const useWebSocketConnection = () => {
   const dispatch = useDispatch();
   const socketRef = useRef<Socket | null>(null);
-  const { page, rowsPerPage } = useSelector(state => state.appState.pagination);
+  const { page, rowsPerPage } = useSelector(
+    (state) => state.appState.pagination
+  );
 
   useEffect(() => {
     const socket = io('http://localhost:8000');
     socketRef.current = socket;
 
+    // Adding logging for connection status for easy validation production code should not have console logs
     socket.on('sensor-status-update', (payload) => {
+      console.log('sensor-status-update:', JSON.stringify(payload));
       dispatch(AppAction.updateSensorData(payload));
     });
 
--- client/src/slices/app-state.ts
+++ client/src/slices/app-state.ts
@@ -64,7 +64,7 @@ export const reducers = {
       payload: {
         serial: string;
         version: string;
-        isoOnline: boolean;
+        isOnline: boolean;
       };
     }
   ): void {
